3c8b6969018a84b032d3e2f121d85c2d08cc73e0
crazycattle <838872031@qq.com>
2018-10-22 11:07:34 +0800

    fix my course page

--- pages/myCourse/course.js
+++ pages/myCourse/course.js
@@ -1,75 +1,92 @@
-// pages/myCourse/course.js
+import {
+  getCollect
+} from '../../api.js'
+
+import {
+  setNewToken,
+  initLoginStatus,
+  getUserState,
+  navToLogin
+} from '../../utils/util'
+
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    list: [
-      { img: 'https://static.dazhao100.cn/pic/1526907753l016152041.png', title: 'UI设计中的插画与情感化hahhahahhhhahah' },
-      { img: 'https://static.dazhao100.cn/pic/1526907753l016152041.png', title: 'UI设计中的插画与情感化hahhahahhhhahah' },
-      { img: 'https://static.dazhao100.cn/pic/1526907753l016152041.png', title: 'UI设计中的插画与情感化hahhahahhhhahah' },
-      { img: 'https://static.dazhao100.cn/pic/1526907753l016152041.png', title: 'UI设计中的插画与情感化hahhahahhhhahah' },
-      { img: 'https://static.dazhao100.cn/pic/1526907753l016152041.png', title: 'UI设计中的插画与情感化hahhahahhhhahah' },
-      { img: 'https://static.dazhao100.cn/pic/1526907753l016152041.png', title: 'UI设计中的插画与情感化hahhahahhhhahah' },
-      { img: 'https://static.dazhao100.cn/pic/1526907753l016152041.png', title: 'UI设计中的插画与情感化hahhahahhhhahah' },
-      { img: 'https://static.dazhao100.cn/pic/1526907753l016152041.png', title: 'UI设计中的插画与情感化hahhahahhhhahah' }
-    ]
+    curpage: 1,
+    list: [],
+    canLoadMore: true,
+    showLoading: true,
+    showTips: false,
   },
+  lower() {
+    const _self = this
+    if (_self.timer) {
+      clearTimeout(_self.timer)
+    }
+    _self.timer = setTimeout(() => {
+      _self.initGetData()
+    }, 500)
+  },
+  initGetData () {
+    let _self = this
+    let loginType = wx.getStorageSync('loginType')
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+    if (this.data.canLoadMore) {
+      this.setData({
+        showLoading: true
+      })
+      wx.request({
+        url: `${getCollect}?stu_id=${app.globalData.student_id}&token=${app.globalData.token}&p=${this.data.curpage}`,
+        success: res => {
+          console.log(res)
+          if (res.data.tokeninc == '0') {
+            if (loginType == 'wxlogin') {
+              setNewToken().then(res => {
+                if (res == 'ok') {
+                  _self.initGetData()
+                }
+              })
+            } else {
+              initLoginStatus()
+            }
+          } else {
+            if (res.data.error == '0') {
+              let data = res.data.listjson
+              if (data.length > 0) {
+                if (data.length == 10) {
+                  this.setData({
+                    curpage: ++this.data.curpage,
+                    canLoadMore: true
+                  })
+                } else {
+                  this.setData({
+                    canLoadMore: false
+                  })
+                }
+                this.setData({
+                  showLoading: false,
+                  list: this.data.list.concat(data)
+                })
+              } else {
+                this.setData({
+                  showTips: !this.data.showTips
+                })
+              }
+            }
+          }
+        }
+      })
+    }
+  },
   onLoad: function (options) {
-
+    this.initGetData()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  onReady: function () {},
+  onShow: function () {},
+  onHide: function () {},
+  onUnload: function () {},
+  onPullDownRefresh: function () {},
+  onReachBottom: function () {},
+  onShareAppMessage: function () {}
 })
